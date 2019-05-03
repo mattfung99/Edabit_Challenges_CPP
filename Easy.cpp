df9eed1c9991f027c439285e83769df4ace8bd3a
Matt Fung <official.matthiasfung@gmail.com>
2019-05-02 18:55:14 -0700

    May 2, 2019 Update

--- Easy.cpp
+++ Easy.cpp
@@ -165,3 +165,127 @@ std::vector<int> bbqSkewers(std::vector<std::string> grill) {
 /*
 ** Problem 12: Sum of Digits Between Two Numbers
 */
+int sumDigits(int a, int b)
+{
+	int sum{0}, i{a};
+	while (a <= b) {
+		do {
+			sum += i % 10;
+			i /= 10;
+		} while (i > 0);
+		a++;
+		i = a;
+	}
+	return sum;
+}
+
+/*
+** Problem 13: Index Shuffle
+*/
+std::string indexShuffle(std::string str) {
+	std::string myStr;
+	for (int i = 0; i < str.length(); i += 2) 
+		myStr += str[i];
+	for (int j = 1; j < str.length(); j += 2)
+		myStr += str[j];
+	return myStr;
+}
+
+/*
+** Problem 14: X and Y Coordinates
+*/
+std::vector<std::vector<int>> convertCartesian(std::vector<int> x, std::vector<int> y) {
+	std::vector<std::vector<int>> my2DArr;
+	for (int i = 0; i < x.size(); i++) {
+		my2DArr[i][0] = x[i];
+		my2DArr[i][1] = y[i];		// I made a mistake on my solution on Edabit... it should be my2DArr[i][1] instead of my2DArr[i][0]
+	}
+	return my2DArr;
+}
+
+/*
+** Problem 15: Return the Factorial
+*/
+int factorial(int n) {
+	int fac{ 1 };
+	for ( ; n > 0; n--) 
+		fac *= n;
+	return fac;
+}
+// OR (Recursion)
+int factorial(int n) {
+	return (n <= 1) ? 1 : n * factorial(n - 1);
+}
+
+/*
+** Problem 16: Little Dictionary
+*/
+std::vector<std::string>
+dictionary(std::string initial, std::vector<std::string> words) {
+	std::vector<std::string> lDict;
+	for (int i = 0; i < words.size(); i++) {
+		for (int j = 0; j < initial.length(); j++) {
+			if (initial[j] != words[i].at(j))
+				continue;
+			lDict[i] == words[i];
+		}
+	}
+	return lDict;
+}
+
+/*
+** Problem 17: Chat Room Status
+*/
+std::string chatroomStatus(std::vector<std::string> users) {
+	switch(users.size()) {
+		case 0:
+			return "no one online";
+		case 1:
+			return users[0] + " online";
+		case 2:
+			return users[0] + " and " + users[1] + " online";
+		default:
+			return users[0] + ", " + users[1] + " and " + std::to_string(users.size() - 2) + " more online"; 
+	}
+}
+
+/*
+** Problem 18: State Names and Abbreviations
+*/
+std::vector<std::string> filterStateNames(std::vector<std::string> states, std::string type) {
+	std::vector<std::string> stateList;
+	for (int i = 0; i < states.size(); i++) {
+		if (states[i].length() == 2 && type[0] == 'a')
+			stateList[i] = states[i];
+		if (states[i].length() > 2 && type[0] == 'f')
+			stateList[i] = states[i];
+	} 
+	return stateList;
+}
+
+/*
+** Problem 19: Convenience Store
+*/
+bool changeEnough(std::vector<int> change, int amountDue) {
+	return float(change[0] * .25 + change[1] * .10 + change[2] * 0.05 + change[3] * 0.01) > amountDue ? true : false;
+}
+
+/*
+** Problem 20: The Museum of Incredibly Dull Things
+*/
+std::vector<int> removeSmallest(std::vector<int> arr) {
+	int min{ arr[0] }, index{ 0 };
+	std::vector<int> newArr;
+	for (int i = 0; i < arr.size(); i++) {
+		if (min > arr[i]) {
+			min = arr[i];
+			index = i;
+			continue;
+		}
+		newArr[i] = arr[i];
+	}
+	for (int j = index; j < arr.size(); j++) 
+		newArr[j] = arr[j + 1];
+	return newArr;
+}
+
